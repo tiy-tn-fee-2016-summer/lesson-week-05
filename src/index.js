9d9e3366b6ddefefce83e3f2f29fa032991f0036
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-11 10:13:36 -0500

    Exercise code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-'use strict';
+export default function() {
+  const people = [
+    {
+      name: 'Carmine Braun',
+      age: 6,
+    },
+    {
+      name: 'Gardner Hansen',
+      age: 56,
+    },
+    {
+      name: 'Robert Wuckert',
+      age: 21,
+    },
+    {
+      name: 'Damon Barton',
+      age: 31,
+    },
+    {
+      name: 'Vaughn Will',
+      age: 39,
+    },
+  ];
+
+
+  // Find the oldest object in "people" and put the name into the "h2" element on the page
+
+  let oldestPerson = people[0];
+
+  // Loop through our people
+  for (let i = 0; i < people.length; i++) {
+    // Check to see if the current looped person is older than the oldest person so far
+    if (people[i].age > oldestPerson.age) {
+      // Change the oldest person we've found
+      oldestPerson = people[i];
+    }
+
+    console.log('i: ', i, 'oldest: ', oldestPerson);
+  }
+
+  // Fill in h2 with the oldest person's name!
+  const elm = document.querySelector('h2');
+  elm.innerText = oldestPerson.name;
+}
