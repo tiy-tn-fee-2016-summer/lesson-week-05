3756a56478c7aecfae292bea9eef6cd1a0ca01d2
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-11 11:10:50 -0500

    Reduce oldestPerson

--- src/index.js
+++ src/index.js
@@ -27,8 +27,6 @@ export default function (listEl) {
 
   // Find the oldest object in "people" and put the name into the "h2" element on the page
 
-  let oldestPerson = people[0];
-
   const renderPersonItem = (person) => {
     const item = new PersonItem(person);
     item.render();
@@ -51,18 +49,19 @@ export default function (listEl) {
 
   console.log(onlyAges);
 
-  // Loop through our people
-  for (let i = 0; i < people.length; i++) {
-    // Check to see if the current looped person is older than the oldest person so far
-    if (people[i].age > oldestPerson.age) {
-      // Change the oldest person we've found
-      oldestPerson = people[i];
+  function whosOlder(a, b) {
+    if (a.age > b.age) {
+      return a;
     }
 
-    // console.log('i: ', i, 'oldest: ', oldestPerson);
+    return b;
   }
+
+  const oldestPerson = people.reduce(whosOlder);
 
   // Fill in h2 with the oldest person's name!
   const elm = document.querySelector('h2');
   elm.innerText = oldestPerson.name;
+
+  console.log(JSON.stringify(people, null, 2));
 }
