9aa5ba69064902555d38750354615ec310e2ce1a
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-12 14:57:39 -0500

    Search for array of pokemonz

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-'use strict';
+export default class App {
+  constructor(appElement) {
+    this.appElement = appElement;
+  }
+
+  start() {
+    // I want to get an array of a bunch of Pokemon from the API
+    fetch('https://pokeapi.co/api/v2/pokemon')
+      // Turing string info from server into Javascript Objects
+      .then((res) => res.json())
+      .then((data) => {
+        // What should we do with dez Pokemon?
+        data.results.forEach((pokemon) => {
+          console.log(pokemon);
+        });
+      });
+  }
+}
