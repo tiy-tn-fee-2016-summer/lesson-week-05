bc677c8fba2630a0db0f52d9cd4fd9aa8b57d5a9
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-14 11:45:46 -0500

    Use map and foreach to create list item views

--- src/question-list.js
+++ src/question-list.js
@@ -1,3 +1,25 @@
+class QuestionListItem {
+  constructor(data) {
+    this.data = data;
+
+    this.element = document.createElement('li');
+    this.element.classList.add('question-list__item');
+    this.element.innerHTML = `<div class="question-card">
+      <h3 class="question-card__checkbox">
+        <i class="fa fa-fw"></i>
+      </h3>
+      <h3 class="question-card__title"></h3>
+      <h3 class="question-card__delete">
+        <i class="fa fa-spin fa-trash"></i>
+      </h3>
+    </div>`;
+  }
+
+  render() {
+    this.element.querySelector('.question-card__title').innerHTML = this.data.value;
+  }
+}
+
 export default class QuestionList {
   constructor(element) {
     this.element = element;
@@ -9,13 +31,13 @@ export default class QuestionList {
   }
 
   render() {
-    this.data.map((question) => {
-      const el = document.createElement('li');
-      el.innerHTML = question.value;
+    // this.element.innerHTML = '';
 
-      return el;
-    }).forEach((alsoEl) => {
-      this.element.appendChild(alsoEl);
-    });
+    this.data.map((question) => new QuestionListItem(question))
+      .forEach((questionItem) => {
+        questionItem.render();
+
+        this.element.appendChild(questionItem.element);
+      });
   }
 }
